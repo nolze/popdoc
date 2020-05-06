73a2b77143b392a18401597aa41e951799078638
nolze <nolze@archlinux.us>
2020-05-06 20:28:56 +0900

    Allow raw html

--- builder/default.js
+++ builder/default.js
@@ -9,13 +9,15 @@ const remark2rehype = require('remark-rehype');
 const doc = require('rehype-document');
 const format = require('rehype-format');
 const html = require('rehype-stringify');
+const raw = require('rehype-raw');
 const slug = require('rehype-slug');
 const wrap = require('rehype-wrap');
 
 function build(srcFile, dstFile, { matter }) {
   let processor = remark()
     .data('settings', { footnotes: matter.footnotes === false ? false : true })
-    .use(remark2rehype)
+    .use(remark2rehype, { allowDangerousHTML: true })
+    .use(raw)
     .use(slug)
     .use(doc, {
       js: matter.js,
