8619bd4e8ffe19e968cb004d56139ea93ca9636c
nolze <nolze@users.noreply.github.com>
2023-10-22 17:10:23 +0000

    deploy: a849e1032f020005a51b7c1ae7681d4a1c50895d

---
+++ assets/index.html-7981e222.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-fffb8e28","path":"/guide/","title":"Guide","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1697994576000,"contributors":[{"name":"nolze","email":"nolze@archlinux.us","commits":1}]},"filePathRelative":"guide/README.md"}');export{e as data};
